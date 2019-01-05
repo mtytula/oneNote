a4082e774d4cd9b82ff4edfe58048898a13ee2bc
Marek Tytuła <tytmarek@gmail.com>
2019-01-05 17:52:42 +0100

    css, devise, haml implementation and other stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users
+  get 'home/index'
+
+  authenticated :user do
+    resources :notes
+    root 'notes#index', as: "authenticated_root"
+  end
+
+  root 'home#index'
+
 end
